f7bafd1b84d1728170ff6f2076fdb6d7bc76cf85
Rafael Dutra <raffaeldutra@gmail.com>
2018-01-25 01:18:57 -0200

    Removed while true and fixed bash header

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 if [ "${OSTYPE}"  = "msys" ]; then
     declare url="http://192.168.99.100"
 else
     declare url="http://localhost:1313"
 fi
+
+echo $url
 
 if [ "${1}" = "-p" ]; then
     /go/bin/hugo \
@@ -17,20 +19,15 @@ if [ "${1}" = "-p" ]; then
     --destination /src/public
 
 elif [ "${1}" = "-s" ]; then
-    while [ true ]
-    do
-        /go/bin/hugo server \
-        --watch true \
-        --bind 0.0.0.0 \
-        --config /src/config.toml \
-        --layoutDir /src/layouts \
-        --themesDir /src/themes \
-        --contentDir /src/content \
-        --ignoreCache \
-        --disableFastRender \
-        --config /src/config.toml \
-        --baseURL ${url}
-        
-        sleep 1
-    done
+    /go/bin/hugo server \
+    --watch true \
+    --bind 0.0.0.0 \
+    --config /src/config.toml \
+    --layoutDir /src/layouts \
+    --themesDir /src/themes \
+    --contentDir /src/content \
+    --ignoreCache \
+    --disableFastRender \
+    --config /src/config.toml \
+    --baseURL ${url}
 fi
